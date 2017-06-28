3f038f3ca04cfad7aef811f57d4e41d1d5969c46
Marco Paco <marcopaco1230@gmail.com>
2017-06-28 20:33:30 +0800

    bug fix

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -12,7 +12,7 @@ class PusherController extends Controller
 {
     public function auth(Request $request)
     {
-    	$pusher = new Pusher(env('PUSHER_KEY'), env('PUSHER_SECRET'), env('PUSHER_APP_ID')));
+    	$pusher = new Pusher(env('PUSHER_KEY'), env('PUSHER_SECRET'), env('PUSHER_APP_ID'));
     	
     	echo $pusher->socket_auth($request->channel_name, $request->socket_id);
     }
